a31d6d496facf34032068f126889096cb109216a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-27 14:22:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,9 @@ class School
   
   def add_student(name, grade)
     if roster.has_key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] << name
     #NEW["grade"]
     #= []
     #@new["grade"] << "name"
